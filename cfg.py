ab29d1a3b6cd7a0c013302c913d5395f1c414e12
Segfaul <101005929+Segfaul@users.noreply.github.com>
2023-05-09 20:45:18 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,5 +1,9 @@
 web_url = 'https://google.com'
-user_agents: [str] = []
+user_agents: [str] = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit'
+    '/538 (KHTML, like Gecko) Chrome/36 Safari/538',
+    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2599.0 Safari/537.36'
+]
 american_proxy = ''
 tg_api_token = ''
 tg_own_id = 1
